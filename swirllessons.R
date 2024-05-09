55ebb46057e49f85278e1861b01c25ce147ebccb
Asude Berber <asudeberber@hotmail.com>
2024-05-09 07:40:14 +0200

    Add files via upload

---
+++ swirllessons.R
@@ -0,0 +1,16 @@
+swirl()
+library(swirl)
+swirl()
+A
+
+#k means clustering
+swirl()
+k-means method "aims to partition the points into
+| k groups such that the sum of squares from points to the assigned cluster centres
+| is minimized."
+#k-means clustering requires some distance metric (say Euclidean)
+#R function tapply which applies "a function over a ragged array".
+#| This means that every element of the array is assigned a factor and the function is
+#| applied to subsets of the array (identified by the factor vector). This allows us
+#| to take advantage of the factor vector newClust we calculated.
+
